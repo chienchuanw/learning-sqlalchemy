552052d8402b2a276deb9a5e751ec5ea5e33e3c9
chienchuanw <chienchuanwww@gmail.com>
2024-07-14 00:08:56 +0800

    Chapter: How to use 'where' to filter when selecting data with 'select' statement

--- select_data.py
+++ select_data.py
@@ -15,3 +15,25 @@ with engine.connect() as connect:
         # Print each row from the result set
         print(row)
         print(f"{row.name} - {row.full_name}")
+
+
+# Print the columns of the users_table
+print(users_table.columns)
+
+# Print the columns of the users_table using the 'c' attribute (an alias for 'columns')
+# Which means 'c' is equal to 'columns'
+print(users_table.c)
+
+# Print the 'full_name' column object of the users_table
+print(users_table.c.full_name)
+
+
+# Use "WHERE" to filter
+# Create a SELECT statement for the users_table with a WHERE clause to filter rows where the name column is "Joe"
+statement = select(users_table).where(users_table.c.name == "Joe")
+
+with engine.connect() as connect:
+    result = connect.execute(statement)
+
+    for row in result:
+        print(row)
